a7608b7d5319607e39ffd4250142a0517354ffd4
itsdo <itsdo@PC-SAMUELACER>
2022-11-24 10:39:36 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICA4_PARIMPAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                    Console.WriteLine("dame un numero y te dire si es par o impar");
+                    int numero = int.Parse(Console.ReadLine());
+                    if (numero == 0)
+                    {
+                        Console.WriteLine("tu numero es par");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("tu numero es impar");
+                    }
+                    Console.ReadKey();
+
+                  }
+        }
+    }
+
